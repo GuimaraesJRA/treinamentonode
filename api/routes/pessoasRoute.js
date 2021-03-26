6d2bcfa0e55eaa2e1fe57823618cf050959d9088
Joao Guimaraes <joao.guimaraes@devexo.com.br>
2021-03-26 16:23:16 -0300

    [ADD] Outros elementos do CRUD read update delete

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -1,9 +1,14 @@
 const { Router } = require('express');
+const PessoaController = require('../controllers/PessoaController');
 const pessoaController = require('../controllers/PessoaController');
 
 
 const router = Router();
 
 router.get('/pessoas', pessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', pessoaController.pegaUmaPessoa);
+router.post('/pessoas', PessoaController.criaPessoa);
+router.put('/pessoas/:id', PessoaController.atualizaPessoa);
+router.delete('/pessoas/:id', PessoaController.apagaPessoa); 
 
 module.exports = router;
